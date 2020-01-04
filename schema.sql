450b09746a65d9e39b7e083d4d49066258dacd20
anicdao <amielnicdao@gmail.com>
2020-01-04 11:56:22 -0500

    added createNewAcc and edited seeds and schema

--- schema.sql
+++ schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE passwords
 (
 	id int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (id),
+	user_name varchar(255) NOT NULL,
 	type varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
     user_id int NOT NULL REFERENCES users(id)
