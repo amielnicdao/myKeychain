b8f2ddbfbab1cea2fbab920743b985ce7f9b9b17
anicdao <amielnicdao@gmail.com>
2020-01-07 21:09:09 -0500

    dynamically added users accounts

--- seeds.sql
+++ seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (name, password) VALUES ('Deion', 'password1');
 SELECT @last := LAST_INSERT_ID();
-INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Deion','Facebook', 'fbPass!', @last);
-INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Christian','Insta', 'instaPass!', @last);
-INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Claudia','Facebook', 'fbPgfcfass!', @last);
-INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Amiel','LinkedIn', 'linkedPass!', @last);
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Deion','Instagram', 'fbPass!', @last);
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Christian','Facebook', 'instaPass!', @last);
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Claudia','Twitter', 'fbPgfcfass!', @last);
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Amiel','Amazon', 'amazonPass!', @last);
