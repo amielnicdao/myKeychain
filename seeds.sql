b287edefe9e83da2a80cfcfb17f166a5cee5f9d6
Christian <chrell827@gmail.com>
2020-01-10 20:00:01 -0500

    add website working

--- seeds.sql
+++ seeds.sql
@@ -4,3 +4,10 @@ INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Deion','Inst
 INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Christian','Facebook', 'instaPass!', @last);
 INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Claudia','Twitter', 'fbPgfcfass!', @last);
 INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Amiel','Amazon', 'amazonPass!', @last);
+
+INSERT INTO users (name, password) VALUES ('SecondUser', 'password2');
+SELECT @last := LAST_INSERT_ID();
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('TEST1','Instagram', 'fbPass!', @last);
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('Test2','Facebook', 'instaPass!', @last);
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('test3','Twitter', 'fbPgfcfass!', @last);
+INSERT INTO passwords (user_name, type, password, user_id) VALUES ('test3','Amazon', 'amazonPass!', @last);
